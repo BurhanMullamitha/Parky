449e5ef7dd1b66cc7ac62801719a17b525875e16
Burhan Mullamitha <burhanmm1998@gmail.com>
2020-06-29 20:00:13 +0100

    Added National Park API operations (GET, POST, PUT, DELETE)

--- ParkyAPI/Repository/NationalParkRepository.cs
+++ ParkyAPI/Repository/NationalParkRepository.cs
@@ -7,55 +7,56 @@ using ParkyAPI.Repository.IRepository;
 
 namespace ParkyAPI.Repository
 {
-    public class NationalParkRepository : INationalParkRepository
+  public class NationalParkRepository : INationalParkRepository
+  {
+    private readonly ApplicationDbContext _db;
+
+    public NationalParkRepository(ApplicationDbContext db)
     {
-        private readonly ApplicationDbContext _db;
-
-        public NationalParkRepository(ApplicationDbContext db)
-        {
-            _db = db;
-        }
-
-        public bool CreateNationalPark(NationalPark nationalPark)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool DeleteNationalPark(NationalPark nationalPark)
-        {
-            _db.NationalParks.Remove(nationalPark);
-            return Save();
-        }
-
-        public NationalPark GetNationalPark(int nationalParkId)
-        {
-            return _db.NationalParks.FirstOrDefault(a => a.Id == nationalParkId);
-        }
-
-        public ICollection<NationalPark> GetNationalParks()
-        {
-            return _db.NationalParks.OrderBy(a => a.Name).ToList();
-        }
-
-        public bool NationalParkExists(string name)
-        {
-            return _db.NationalParks.Any(a => a.Name.ToLower().Trim() == name.ToLower().Trim());
-        }
-
-        public bool NationalParkExists(int id)
-        {
-            return _db.NationalParks.Any(a => a.Id == id);
-        }
-
-        public bool Save()
-        {
-            return _db.SaveChanges() > 0 ? true : false;
-        }
-
-        public bool UpdateNationalPark(NationalPark nationalPark)
-        {
-            _db.NationalParks.Update(nationalPark);
-            return Save();
-        }
+      _db = db;
     }
+
+    public bool CreateNationalPark(NationalPark nationalPark)
+    {
+      _db.NationalParks.Add(nationalPark);
+      return Save();
+    }
+
+    public bool DeleteNationalPark(NationalPark nationalPark)
+    {
+      _db.NationalParks.Remove(nationalPark);
+      return Save();
+    }
+
+    public NationalPark GetNationalPark(int nationalParkId)
+    {
+      return _db.NationalParks.FirstOrDefault(a => a.Id == nationalParkId);
+    }
+
+    public ICollection<NationalPark> GetNationalParks()
+    {
+      return _db.NationalParks.OrderBy(a => a.Name).ToList();
+    }
+
+    public bool NationalParkExists(string name)
+    {
+      return _db.NationalParks.Any(a => a.Name.ToLower().Trim() == name.ToLower().Trim());
+    }
+
+    public bool NationalParkExists(int id)
+    {
+      return _db.NationalParks.Any(a => a.Id == id);
+    }
+
+    public bool Save()
+    {
+      return _db.SaveChanges() > 0 ? true : false;
+    }
+
+    public bool UpdateNationalPark(NationalPark nationalPark)
+    {
+      _db.NationalParks.Update(nationalPark);
+      return Save();
+    }
+  }
 }
